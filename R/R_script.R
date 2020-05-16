9d60e65bb0226e43e354b50252596cb9c3fc8d7a
Patrick Leung <patrick.leung819@gmail.com>
2020-05-17 00:22:06 +1000

    update

--- R/R_script.R
+++ R/R_script.R
@@ -1,1 +1,30 @@
 ## Script
+
+##
+library(tidyverse)
+library(data.table)
+library(tidylog)
+library(lubridate)
+library(Hmisc)
+library(quantmod)
+
+##
+
+AAPL <- as.data.frame(getSymbols("AAPL", env=NULL))
+
+AAPL %>% glimpse
+
+AAPL<- AAPL %>% mutate(Date = ymd(row.names(AAPL)))
+
+AAPL %>% ggplot(aes(x=Date, y=AAPL.Close))+
+  geom_line()
+
+
+##
+APT<- as.data.frame(getSymbols("APT.AX", env=NULL))
+
+APT<- APT %>% mutate(Date = ymd(row.names(APT)))
+
+APT %>% view()
+APT %>% ggplot(aes(x=Date, y=APT.AX.Close))+
+  geom_line()
